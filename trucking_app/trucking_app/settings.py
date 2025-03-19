dee165b3226771a4da7bc6ad5d54023eeb44f22a
Ebiperre Iyoro <iyorop@yahoo.com>
2025-03-19 22:48:45 +0100

    Fixed cors

--- trucking_app/trucking_app/settings.py
+++ trucking_app/trucking_app/settings.py
@@ -51,7 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 # Enable CORS for the React app
